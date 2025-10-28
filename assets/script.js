7ea90b7d76f7847e61fd51dce6a82bede46cc553
norwaymalayali <norwaymalayali@gmail.com>
2025-10-28 14:07:01 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,4 @@
 function setLanguage(lang) {
-  // Automatically detect your repo path (e.g., /nm_blog/)
-  const repoName = 'nm_blog'; // or change if your repo name changes later
-  const base = `/${repoName}/`;
-
-  if (lang === 'en') {
-    window.location.href = base + 'en/index.html';
-  } else if (lang === 'ml') {
-    window.location.href = base + 'ml/index.html';
-  }
+  if (lang === 'en') window.location.href = 'nm_blog/en/index.html';
+  else window.location.href = 'nm_blog/ml/index.html';
 }
