3a8802407785db1a6ac5c09e20dcbb8da917f352
norwaymalayali <norwaymalayali@gmail.com>
2025-10-28 16:38:46 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,6 @@
 function setLanguage(lang) {
-  // This detects your current base path (e.g., /nm_blog/)
-  const currentPath = window.location.pathname;
-  const repoPath = currentPath.split('/')[1]; // "nm_blog"
-  const base = '/' + repoPath + '/';
+  const base = window.location.origin; // https://www.norwaymalayali.com
 
-  if (lang === 'en') {
-    window.location.href = base + 'en/index.html';
-  } else if (lang === 'ml') {
-    window.location.href = base + 'ml/index.html';
-  }
+  if (lang === 'en') window.location.href = base + '/en/index.html';
+  else if (lang === 'ml') window.location.href = base + '/ml/index.html';
 }
